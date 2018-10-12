cd2cb6404d2b230fbff0889c5ac1a0931d6306d5
qingpengtan <tang1994year@163.com>
2018-10-12 17:03:32 +0800

    图片接口

---
+++ src/main/java/com/example/config/UploadProperties.java
@@ -0,0 +1,54 @@
+package com.example.config;
+
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("upload")
+public class UploadProperties {
+    // 获取存放位置
+    private Map<String, String> localtion;
+
+    // 单个文件大小
+    private String maxFileSize;
+
+    // 单次上传总文件大小
+    private String maxRequestSize;
+
+    public Map<String, String> getLocaltion() {
+        return localtion;
+    }
+
+    public void setLocaltion(Map<String, String> localtion) {
+        this.localtion = localtion;
+    }
+
+    public String getMaxFileSize() {
+        return maxFileSize;
+    }
+
+    public void setMaxFileSize(String maxFileSize) {
+        this.maxFileSize = maxFileSize;
+    }
+
+    public String getMaxRequestSize() {
+        return maxRequestSize;
+    }
+
+    public void setMaxRequestSize(String maxRequestSize) {
+        this.maxRequestSize = maxRequestSize;
+    }
+
+    public String getBasePath() {
+        String location = "";
+        String os = System.getProperty("os.name");
+        if(os.toLowerCase().startsWith("win")) {
+            location = this.getLocaltion().get("windows");
+        } else {
+            location = this.getLocaltion().get("linux");
+        }
+        return location;
+    }
+}
--- src/main/java/com/example/controller/system/UserControll.java
+++ src/main/java/com/example/controller/system/UserControll.java
@@ -1,77 +1,116 @@
 package com.example.controller.system;
 
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
-import com.example.config.util.MD5Util;
+import com.example.config.UploadProperties;
+import com.example.config.util.CodeMsg;
 import com.example.config.util.Result;
+import com.example.controller.IndexControl;
 import com.example.entity.UserAccount;
 import com.example.service.impl.UserServiceImpl;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.*;
 
 @RestController
 @RequestMapping("/sys/user")
 public class UserControll {
 
+    private final static Logger log = LoggerFactory.getLogger(UserControll.class);
+
     @Autowired
     UserServiceImpl userService;
 
-//    @RequestMapping(value = "/test", method = RequestMethod.POST)
-//    public void test(HttpServletRequest request) {
-//        HashMap map = new HashMap();
-//        map.put("province",request.getParameter("province"));
-//        map.put("city",request.getParameter("city"));
-//        map.put("area",request.getParameter("area"));
-//        map.put("value",request.getParameter("value"));
-//        map.put("parent",request.getParameter("parent"));
-//        map.put("short",request.getParameter("short"));
-//        userService.tests(map);
+    @Resource
+    private ResourceLoader resourceLoader;
+    @Resource
+    private UploadProperties uploadProperties;
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+//    public Result test(HttpServletRequest request,@RequestParam("img") MultipartFile myfil) throws IOException {
+////        String filePath = request.getSession().getServletContext().getRealPath("imgupload/");
+//        File file = new File("D:\\24.png");
+//        OutputStream outputStream = new FileOutputStream(file);
+////        myfile.transferTo(file);
+//        IOUtils.copy(myfil.getInputStream(),outputStream);
+//        return Result.success("D:\\24.png");
 //    }
+    public Result upload(@RequestParam("img") MultipartFile file, HttpServletRequest request) {
+        // 获取文件存放路径
+        String basePath = uploadProperties.getBasePath();
+        String location = new Date().getTime() + "/";
+        // 判断文件夹是否存在，不存在则
+        File targetFile = new File(basePath +"yy.png");
+//        if (!targetFile.exists()) {
+//            targetFile.mkdirs();
+//        }
+
+            String fileName = file.getOriginalFilename();
+            try {
+                OutputStream outputStream = new FileOutputStream(targetFile);
+//        myfile.transferTo(file);
+                IOUtils.copy(file.getInputStream(), outputStream);
+
+                org.springframework.core.io.Resource resource =resourceLoader.getResource("file:" + Paths.get(uploadProperties.getBasePath() +"yy.png").toString());
+
+                return Result.success(resource.getFilename());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return Result.error(CodeMsg.ACCOUNT_QUIT);
+    }
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public Result login(HttpServletRequest request, HttpServletResponse response, UserAccount userAccount) {
         userAccount.setRoleId(3);
-        Map map = userService.login(request,response, userAccount);
-        return  Result.success(map);
+        Map map = userService.login(request, response, userAccount);
+        return Result.success(map);
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.POST)
     public Result List(HttpServletRequest request, HttpServletResponse response, UserAccount userAccount) {
-        String pageN= request.getParameter("page");
-        if(StringUtils.isEmpty(pageN)){
+        String pageN = request.getParameter("page");
+        if (StringUtils.isEmpty(pageN)) {
             pageN = "1";
         }
         Page page = new Page();
         page.setCurrent(Integer.parseInt(pageN));
         page.setSize(10);
-        List userList = userService.selectUserList(page,userAccount);
+        List userList = userService.selectUserList(page, userAccount);
         page.setRecords(userList);
         HashMap map = new HashMap();
-        map.put("totalPage",page.getPages());
-        map.put("current",page.getCurrent());
-        map.put("totalSize",page.getTotal());
-        map.put("userList",page.getRecords());
-        return  Result.success(map);
+        map.put("totalPage", page.getPages());
+        map.put("current", page.getCurrent());
+        map.put("totalSize", page.getTotal());
+        map.put("userList", page.getRecords());
+        return Result.success(map);
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public Result save(HttpServletRequest request, HttpServletResponse response, UserAccount userAccount) {
         userService.save(userAccount);
-        return  Result.success(null);
+        return Result.success(null);
     }
 
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public Result delete(HttpServletRequest request, HttpServletResponse response) {
         String id = request.getParameter("id");
         userService.deleteById(Integer.parseInt(id));
-        return  Result.success(null);
+        return Result.success(null);
     }
 }
