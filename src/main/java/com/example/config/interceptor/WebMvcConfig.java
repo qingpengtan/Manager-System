63db968060e957994d81f99489c88deada06d5ad
qingpengtan <tang1994year@163.com>
2019-03-08 09:22:02 +0800

    跨域处理，修复文章状态

--- src/main/java/com/example/config/interceptor/WebMvcConfig.java
+++ src/main/java/com/example/config/interceptor/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -39,6 +40,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 				.excludePathPatterns("/sys/user/login","/sys/user/test");
 
 		super.addInterceptors(registry);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**");
 	}
 
 
