51c8bb747b9ba483897db06cf40164ba4e81dbdd
qingpengtan <tang1994year@163.com>
2019-03-25 10:51:24 +0800

    修复文章保存bug

--- src/main/java/com/example/entity/Article.java
+++ src/main/java/com/example/entity/Article.java
@@ -34,9 +34,6 @@ public class Article {
     @TableField("view_num")
     private Integer viewNum;
 
-    @TableField("parent_id")
-    private String parentId;
-
     private String content;
 
     private String status;
@@ -86,14 +83,6 @@ public class Article {
 
     public void setArticleTitle(String articleTitle) {
         this.articleTitle = articleTitle == null ? null : articleTitle.trim();
-    }
-
-    public String getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(String parentId) {
-        this.parentId = parentId;
     }
 
     public String getContent() {
